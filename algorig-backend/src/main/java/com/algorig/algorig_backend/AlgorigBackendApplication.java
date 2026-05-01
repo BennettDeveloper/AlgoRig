7eef0272f0798871dea93ec5a242ee2440b9b650
BennettDeveloper <bennettdeveloper@gmail.com>
2026-05-01 11:33:15 -0400

    feat: setup SpringBoot backend

---
+++ algorig-backend/src/main/java/com/algorig/algorig_backend/AlgorigBackendApplication.java
@@ -0,0 +1,13 @@
+package com.algorig.algorig_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorigBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorigBackendApplication.class, args);
+	}
+
+}
